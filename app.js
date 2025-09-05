c22e4c8defb7efc73e3dd847db870c86206ca2a2
heidarosmalldoll80 <riyensarahh@hotmail.com>
2025-09-05 13:14:57 -0500

    Update server port to 4000 for development.

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
