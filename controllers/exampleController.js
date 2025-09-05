7aa1991263df9ffbc59afb5e6c52869a215b5628
heidarosmalldoll80 <riyensarahh@hotmail.com>
2025-09-05 13:13:10 -0500

    feat: initial commit for controllers/exampleController.js

---
+++ controllers/exampleController.js
@@ -0,0 +1,7 @@
+const config = require('../config/config');
+
+exports.getExampleData = (req, res) => {
+    // Placeholder für die Anbindung an einen KI-Dienst
+    const data = { example: 'Beispiel-Daten von einem KI-Dienst' };
+    res.json(data);
+};
